69c68d82433c9cae83f28976f072c3f36aaf5b0c
wangfuhai <wangfuhai@gmail.com>
2013-11-15 18:19:03 +0800

    增加decorators扩展支持

--- lib/ums/engine.rb
+++ lib/ums/engine.rb
@@ -1,5 +1,10 @@
 module Ums
   class Engine < ::Rails::Engine
     isolate_namespace Ums
+    config.to_prepare do
+      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+        require_dependency(c)
+      end
+    end
   end
 end
