91698be68a99a4e45dd83b347365240f59f351c3
wangfuhai <wangfuhai@gmail.com>
2014-06-17 22:01:03 +0800

    修改扩展类命名规则

--- lib/ums/engine.rb
+++ lib/ums/engine.rb
@@ -2,7 +2,7 @@ module Ums
   class Engine < ::Rails::Engine
     isolate_namespace Ums
     config.to_prepare do
-      Dir.glob(Rails.root + "app/decorators/**/*_decorator*.rb").each do |c|
+      Dir.glob(Rails.root + "app/decorators/**/*.rb").each do |c|
         require_dependency(c)
       end
     end
