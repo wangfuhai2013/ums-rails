b5c19499ffbce42730447462556b1af1ba000bb5
wangfuhai <wangfuhai@gmail.com>
2015-01-04 21:22:43 +0800

    修改操作日志view

--- db/migrate/20141113071900_add_operation_for_ums_logs.rb
+++ db/migrate/20141113071900_add_operation_for_ums_logs.rb
@@ -1,7 +1,7 @@
 class AddOperationForUmsLogs < ActiveRecord::Migration
   def change
   	add_column :ums_logs , :operator, :string
-  	add_column :ums_logs , :model_name, :string  	
+  	add_column :ums_logs , :model_class, :string  	
   	add_column :ums_logs , :model_id, :integer
   end
 end
